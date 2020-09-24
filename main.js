af133d4502e725a8d0c4f55d11d69d2d83e7f5c1
Deontre Hemingway <deontrehemingway@Deontres-MacBook-Pro.local>
2020-09-24 11:03:12 -0400

    assignment

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+function isItLong(str){
+  return "That's a long string!"
+}
 
 
 
+if(isItLong){
+  console.log("That's a long string.")
+}
+
+function isItMedium(str){
+  return "That's a regular sized string!"
+}
+
+if(isItMedium){
+  console.log("That's a regular sized string!")
+}
+
+function isItShort(Str){
+  return "That's a small string!"
+}
+if(isItShort){
+  console.log("That's a small string !")
+}
+
+function howLongIsMyString(str){
+  return "That's a long string!" 
+} 
+
+function instructorHeight(num){
+  return 
+}
 
 
 
